95e8f80834c4f366ea70bf485bc74bc8bf4a76ac
kitsuyy <lozovoi29111995@gmail.com>
2022-10-01 03:09:23 +0400

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Kirill Lozovoi on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
